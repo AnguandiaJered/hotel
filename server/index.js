f7f253fac64323a2ade91cfddcad9e34f045b32c
Jered <anguandiatsandijered03@gmail.com>
2022-03-08 12:16:03 +0100

    l'affichage de donnee en react

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const router = require("./Routes/router");
 const cors = require("cors");
+const bodyParser = require('body-parser');
 require("dotenv").config();
 
 
 
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json());
 app.use(router);
 app.listen(process.env.PORT,(err) =>{
     err ? console.log(err) : console.log("Server running on port "+process.env.PORT);
